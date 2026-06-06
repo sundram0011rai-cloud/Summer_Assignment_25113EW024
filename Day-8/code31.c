e9b6172273392fb72513c1beb070248d71fc180d
sundram0011rai-cloud <sundram0011rai@gmail.com>
2026-06-06 13:01:14 +0530

    Add files via upload

---
+++ Day-8/code31.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    for (int i='A';i<='E';i++) {
+        for (int j='A';j<=i;j++){
+            printf("%c",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
